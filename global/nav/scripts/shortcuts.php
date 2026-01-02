a4b73b248f64285d8307f9cf1accc21d1ee3ccdb
Bilal <88000853+mba08mc@users.noreply.github.com>
2026-01-02 18:07:14 +0000

    updated intel

---
+++ global/nav/scripts/shortcuts.php
@@ -0,0 +1,13 @@
+<shortcuts>
+<term/>
+<search_results>
+<match image="http://images.apple.com/search/shortcuts/images/macmaillogin_1182908488s.png" title=".Mac Mail Login" url="http://webmail.mac.com" copy="Log in to your .Mac email on the web." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/airportexpress_1172641152s.png" title="AirPort Express" url="http://www.apple.com/airportexpress/" copy="Connect to the internet, print, and stream music to any room in your home - all wirelessly." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/airportextreme_1172641035s.png" title="AirPort Extreme" url="http://www.apple.com/airportextreme/" copy="AirPort Extreme is the next-generation wireless networking solution from Apple." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/aperture_1172641322s.png" title="Aperture" url="http://www.apple.com/aperture/" copy="The first all-in-one post-production tool for photographers." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/displays_1172644391s.png" title="Displays" url="http://www.apple.com/displays/" copy="Apple%u2019s brilliant flat panel displays offer certified color at unbelievable prices." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/finalcutstudio_1179954061s.png" title="Final Cut Studio" url="http://www.apple.com/finalcutstudio/" copy="Go beyond mere editing with tools for real-time video and audio production." category="Products" priority="1"/>
+<match image="http://images.apple.com/search/shortcuts/images/redeemitunesgiftcards_1181464969s.png" title="Redeem iTunes Gift Cards" url="https://phobos.apple.com/WebObjects/MZFinance.woa/wa/prepaidWizard" copy="Redeem your pre-paid or gift card for iTunes." category="iTunes Store" priority="3"/>
+<match image="http://images.apple.com/search/shortcuts/images/ticketmasteritunes_1181465796s.png" title="Ticketmaster + iTunes" url="http://www.apple.com/itunes/ticketmaster/" copy="Free music when you buy concert tickets for any show scheduled between May 28 and Sept. 3." category="iTunes Store" priority="4"/>
+</search_results>
+</shortcuts>
